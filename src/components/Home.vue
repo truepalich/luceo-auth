8e65e00fc905f4ee776b964bd1bd3ce90cfaf828
Stanislav <civictyper13@gmail.com>
2019-10-03 12:59:51 +0300

    Small fix

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -10,10 +10,10 @@
       <li><router-link :to="{ name: 'SignUpStep3' }">Sign Up. Step 3</router-link></li>
       <li><router-link :to="{ name: 'Profile' }">Profile</router-link></li>
 
-      <li><a target="_blank" href="https://github.com/truepalich/luceo-auth/tree/master/static/email/email_confirm.html">Confirm email</a></li>
-      <li><a target="_blank" href="https://github.com/truepalich/luceo-auth/tree/master/static/email/email_recover_password.html">Recover password</a></li>
-      <li><a target="_blank" href="https://github.com/truepalich/luceo-auth/tree/master/static/email/email_invintation.html">Invintation email</a></li>
-      <li><a target="_blank" href="https://github.com/truepalich/luceo-auth/tree/master/static/email/email_notification.html">Notification email</a></li>
+      <!--<li><a target="_blank" href="https://github.com/truepalich/luceo-auth/tree/master/static/email/email_confirm.html">Confirm email</a></li>-->
+      <!--<li><a target="_blank" href="https://github.com/truepalich/luceo-auth/tree/master/static/email/email_recover_password.html">Recover password</a></li>-->
+      <!--<li><a target="_blank" href="https://github.com/truepalich/luceo-auth/tree/master/static/email/email_invintation.html">Invintation email</a></li>-->
+      <!--<li><a target="_blank" href="https://github.com/truepalich/luceo-auth/tree/master/static/email/email_notification.html">Notification email</a></li>-->
 
       <!--<li><router-link :to="{ name: 'EmailTemplates' }">Email Templates</router-link></li>-->
       <!--<li><a href="../assets/email/email.html">Email Templates</a></li>-->
